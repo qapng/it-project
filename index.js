2e71a659338e2bea1495c1751edc5e86bbcba9e6
qapng <quanganhphucnguyen@gmail.com>
2021-09-04 21:49:15 +0700

    added start command

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const authRouter = require("./routes/authRoutes");
+const authRouter = require('./routes/authRoutes');
 
 // connect to database (TESTING COMMIT)
 const connectDB = async () => {
@@ -11,11 +11,11 @@ const connectDB = async () => {
       `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@davinci.gfolr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
+        useUnifiedTopology: true,
+      },
     );
 
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   } catch (error) {
     console.log(error.message);
     process.exit(1);
@@ -26,8 +26,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
